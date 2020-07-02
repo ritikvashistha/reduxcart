cd4d12b4b660c7b34744eb75d2185eb741d0f0a6
ritikvashistha <vashistharitik9@gmail.com>
2020-07-02 16:26:04 +0530

    Updates

---
+++ precache-manifest.79adbdd05baa8ebc520789318d7d326f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdae3679558b449783fd4c91ae700bb",
+    "url": "/reduxcart/index.html"
+  },
+  {
+    "revision": "18a8a00a7ad211e4df5d",
+    "url": "/reduxcart/static/css/main.c463a086.chunk.css"
+  },
+  {
+    "revision": "f8a87a0215f7ca3ebdc2",
+    "url": "/reduxcart/static/js/2.36edcc9b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reduxcart/static/js/2.36edcc9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a8a00a7ad211e4df5d",
+    "url": "/reduxcart/static/js/main.eeca5b0a.chunk.js"
+  },
+  {
+    "revision": "9546af18074254eb4548",
+    "url": "/reduxcart/static/js/runtime-main.ed69218f.js"
+  }
+]);
